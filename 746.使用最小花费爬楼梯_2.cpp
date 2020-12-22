176b381a30d24fa272b14264506d01ff53c3a273
EthanJia <181716254@qq.com>
2020-12-22 14:10:46 +0800

    [746] rename

--- 746.使用最小花费爬楼梯_2.cpp
+++ 746.使用最小花费爬楼梯_2.cpp
@@ -13,9 +13,35 @@ class Solution
 {
 
 public:
+    int memo[1000]; // DP beiwanglu
+
+    Solution()
+    {
+        for (int i = 0; i < 1000; i++)
+            memo[i] = -1;
+    };
+
+    int helper(vector<int> &cost, int i)
+    {
+        if (memo[i] != -1)
+            return memo[i];
+        else
+        {
+            int len = cost.size();
+            if (i == 1 || i == 0)
+                memo[i] = cost[i];
+            else
+                memo[i] = min(helper(cost, i - 1), helper(cost, i - 2)) + cost[i];
+
+            return memo[i];
+        }
+    }
+
     int minCostClimbingStairs(vector<int> &cost)
     {
-        int dp[1000];
+
+        int len = cost.size();
+        return min(helper(cost, len - 1), helper(cost, len - 2));
     }
 };
 // @lc code=end
