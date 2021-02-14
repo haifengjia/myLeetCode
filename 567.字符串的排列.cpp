84445bda14284139c4909f437b3315c82424106c
EthanJia <181716254@qq.com>
2021-02-14 15:05:45 +0800

    fix the vsc prob

---
+++ 567.字符串的排列.cpp
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=567 lang=cpp
+ *
+ * [567] 字符串的排列
+ */
+
+#include <string>
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+// @lc code=start
+class Solution
+{
+public:
+    bool checkInclusion(string s1, string s2)
+    {
+        string s = s1;
+        return s1 == "1";
+    }
+};
+// @lc code=end
---
+++ 765.情侣牵手.cpp
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=765 lang=cpp
+ *
+ * [765] 情侣牵手
+ */
+
+#include <vector>
+#include <string>
+
+using namespace std;
+
+// @lc code=start
+class Solution
+{
+public:
+    int minSwapsCouples(vector<int> &row)
+    {
+        string s = "sss";
+    }
+};
+// @lc code=end
