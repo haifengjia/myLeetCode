0d88144d79b271c9f5dd05fd7d90fa6d581d8dd3
EthanJia <181716254@qq.com>
2021-09-29 22:05:11 +0800

    feat: finish 69; review 3

--- 69.x-的平方根.cpp
+++ 69.x-的平方根.cpp
@@ -4,12 +4,27 @@
  * [69] x 的平方根
  */
 
-// @lc code=start
-class Solution {
-public:
-    int mySqrt(int x) {
+#include <math.h>
 
+// @lc code=start
+class Solution
+{
+public:
+    int mySqrt(int x)
+    {
+        double beg = 1, end = x;
+        double mid = (beg + end) / 2;
+        while (abs(mid * mid - static_cast<double>(x)) >= 1)
+        {
+            if (static_cast<size_t>(mid) * static_cast<size_t>(mid) == x)
+                return mid;
+            else if (mid * mid > static_cast<double>(x))
+                end = mid;
+            else
+                beg = mid;
+            mid = (beg + end) / 2;
+        }
+        return mid;
     }
 };
 // @lc code=end
-
